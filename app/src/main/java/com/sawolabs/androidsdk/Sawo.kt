1de3b9724bc9e35324db8ce1bac5e9074697902c
Prasanta Kakati <prasantakakati1994@gmail.com>
2020-09-18 22:12:01 +0530

    Changed WebSDK url

--- app/src/main/java/com/sawolabs/androidsdk/Sawo.kt
+++ app/src/main/java/com/sawolabs/androidsdk/Sawo.kt
@@ -5,7 +5,7 @@ import android.content.Intent
 
 class Sawo(private val mContext:Context, private val apiKey: String) {
     fun login(identifierType: String, callBackClass: String) {
-        val sawoWebSDKURL = "http://192.168.0.105:3000/?apiKey=$apiKey&identifierType=$identifierType"
+        val sawoWebSDKURL = "https://websdk.sawolabs.com/?apiKey=$apiKey&identifierType=$identifierType"
         val intent = Intent(mContext, LoginActivity::class.java).apply {
             putExtra(SAWO_WEBSDK_URL, sawoWebSDKURL)
             putExtra(CALLBACK_CLASS, callBackClass)
